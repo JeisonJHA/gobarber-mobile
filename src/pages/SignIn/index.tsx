1b864a3ccc1bbf4cd26487cb931a6e99e605252c
Jeison Azevedo <j3ison@gmail.com>
2020-05-03 23:56:30 -0300

    auth hooks

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -15,6 +15,8 @@ import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
+import { useAuth } from '../../hooks/auth';
+
 import getValidationErros from '../../utils/getValidationErros';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -39,29 +41,37 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
 
-  const handleSubmit = useCallback(async (data: SignInFormData) => {
-    try {
-      formRef.current?.setErrors({});
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail é obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().required('Informe a senha'),
-      });
+  const { signIn } = useAuth();
 
-      await schema.validate(data, { abortEarly: false });
-      // await signIn({ email: data.email, password: data.password });
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail é obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Informe a senha'),
+        });
 
-      // history.push('/dashboard');
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const validations = getValidationErros(err);
-        formRef.current?.setErrors(validations);
-        return;
+        await schema.validate(data, { abortEarly: false });
+        await signIn({ email: data.email, password: data.password });
+
+        // history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const validations = getValidationErros(err);
+          formRef.current?.setErrors(validations);
+          return;
+        }
+        Alert.alert(
+          'Erro na autenticação',
+          'Verifique os dados de credencial.'
+        );
       }
-      Alert.alert('Erro na autenticação', 'Verifique os dados de credencial.');
-    }
-  }, []);
+    },
+    [signIn]
+  );
 
   return (
     <>
