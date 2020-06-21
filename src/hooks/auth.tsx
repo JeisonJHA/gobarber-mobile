f19fc3edac65a2610416d799b6b3df468b4b4d0e
Jeison Azevedo <j3ison@gmail.com>
2020-06-21 17:42:04 -0300

    almost there

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -9,9 +9,15 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -20,7 +26,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
   loading: boolean;
@@ -40,6 +46,7 @@ export const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
       setLoading(false);
@@ -54,6 +61,7 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GOBARBER:token', token],
       ['@GOBARBER:user', JSON.stringify(user)],
     ]);
+    api.defaults.headers.authorization = `Bearer ${token}`;
     setData({ token, user });
   }, []);
 
@@ -73,7 +81,7 @@ export function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error('useAuth should be use within an AutProvider');
+    throw new Error('useAuth should be use within an AuthProvider');
   }
 
   return context;
